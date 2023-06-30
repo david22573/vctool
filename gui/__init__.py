14ce9e486d563fa733407b813c2b68027550e6ba
david22573 <davidmiguel22573@gmail.com>
2023-06-29 18:28:12 -0700

    Change ttk to ttkbootstrap

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,26 +1,6 @@
-import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap
+from ttkbootstrap.constants import *
 
+app = ttkbootstrap.ThemedTk(themename="darkly")
 
-class Window:
-    def __init__(self, master: tk.Tk):
-        self.master = master
-        self.master.title("GUI")
-        self.master.geometry("200x200")
-
-        button = tk.Button(self.master, text="Click me!")
-        button.pack(padx=5, pady=5)
-
-        checkbutton = tk.Checkbutton(self.master, text="Check me!")
-        checkbutton.pack(padx=5, pady=5)
-
-        radiobutton = tk.Radiobutton(self.master, text="Radiobutton")
-        radiobutton.pack(padx=5, pady=5)
-
-        scale = tk.Scale(self.master, from_=0, to=10)
-        scale.pack(padx=5, pady=5)
-
-
-root = tk.Tk()
-Window = Window(root)
-root.mainloop()
+open_button = ttk.Button(app, text="Open", command=open_dialog, bootstyle=SUCCESS)
