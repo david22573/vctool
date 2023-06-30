86d3a6690af786880879fcd61b2d109238852bf0
david22573 <davidmiguel22573@gmail.com>
2023-06-29 19:19:53 -0700

    Ttk gui updated to ttkbootstrap

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,16 @@
-import ttkbootstrap
+import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-app = ttkbootstrap.ThemedTk(themename="darkly")
+root = ttk.Window(themename="darkly")
 
-open_button = ttk.Button(app, text="Open", command=open_dialog, bootstyle=SUCCESS)
+def open_dialog():
+    app = ttk.Toplevel(title="My Toplevel")
+    app.mainloop()
+
+open_button = ttk.Button(root, text="Open", command=open_dialog, bootstyle=SUCCESS)
+open_button.pack(side=LEFT, padx=5, pady=10)
+
+exit_button = ttk.Button(root, text="Exit", bootstyle=(INFO, OUTLINE), command=root.destroy)
+exit_button.pack(side=LEFT, padx=5, pady=10)
+
+root.mainloop()
